f5f0840418ba6842c8bfca180c685071d0f41322
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-07-18 16:39:04 +0900

    version 0.2.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ for d in glob.glob("syncha/dat/model/*"):
 
 setuptools.setup(
   name="syncha-cygwin",
-  version="0.2.0",
+  version="0.2.1",
   packages=setuptools.find_packages(),
   data_files=data_files,
   install_requires=install_requires
